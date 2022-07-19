c3b12f73a93fe5bfdd0453694b02bf924d5a2a92
Jinxin Zhou <48702671+jinxinzhou@users.noreply.github.com>
2022-07-19 17:57:42 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,6 @@
 from .resnet import *
+from .wide_resnet import *
+
+__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+           'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
+           'wide_resnet50_2', 'wide_resnet101_2', 'wide_resnet']
